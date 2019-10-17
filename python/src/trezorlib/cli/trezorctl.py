420de0c75c6c27fc72008f918bf4a456a771bdb2
ciny <stanislav.marcinko@satoshilabs.com>
2019-10-17 12:07:41 +0200

    python cli: change option short names

--- python/src/trezorlib/cli/trezorctl.py
+++ python/src/trezorlib/cli/trezorctl.py
@@ -2058,14 +2058,14 @@ def ontology_get_public_key(connect, address, show_display):
     help="BIP-32 path to signing key, e.g. m/44'/888'/0'/0/0",
 )
 @click.option(
-    "-tx",
+    "-t",
     "--transaction",
     type=click.File("r"),
     default="-",
     help="Transaction in JSON format",
 )
 @click.option(
-    "-tr",
+    "-r",
     "--transfer",
     type=click.File("r"),
     default="-",
@@ -2095,14 +2095,14 @@ def ontology_sign_transfer(connect, address, transaction, transfer):
     help="BIP-32 path to signing key, e.g. m/44'/888'/0'/0/0",
 )
 @click.option(
-    "-tx",
+    "-t",
     "--transaction",
     type=click.File("r"),
     default="-",
     help="Transaction in JSON format",
 )
 @click.option(
-    "-wi",
+    "-w",
     "--withdraw_ong",
     type=click.File("r"),
     default="-",
@@ -2134,14 +2134,14 @@ def ontology_sign_withdraw_ong(connect, address, transaction, withdraw_ong):
     help="BIP-32 path to signing key, e.g. m/44'/888'/0'/0/0",
 )
 @click.option(
-    "-tx",
+    "-t",
     "--transaction",
     type=click.File("r"),
     default="-",
     help="Transaction in JSON format",
 )
 @click.option(
-    "-re",
+    "-r",
     "--register",
     type=click.File("r"),
     default="-",
@@ -2175,7 +2175,7 @@ def ontology_sign_ont_id_register(connect, address, transaction, register):
     help="BIP-32 path to signing key, e.g. m/44'/888'/0'/0/0",
 )
 @click.option(
-    "-tx",
+    "-t",
     "--transaction",
     type=click.File("r"),
     required=True,
@@ -2183,8 +2183,8 @@ def ontology_sign_ont_id_register(connect, address, transaction, register):
     help="Transaction in JSON format",
 )
 @click.option(
-    "-aa",
-    "--add_attr",
+    "-a",
+    "--add_attribute",
     type=click.File("r"),
     required=True,
     default="-",
